cf1045150e4654f5286445a9d3f77f5ea5aff3ad
Ubuntu <ubuntu@ip-172-31-88-125.ec2.internal>
2022-06-04 14:42:11 +0000

    Creating pozuelo-devops which contains deployment scripts for jenkins and python

---
+++ pozuelo-devops/installjenkins.sh
@@ -0,0 +1,23 @@
+## Script Jenkis deployment
+## Jorge Pozuelo
+
+# Actualizar listado de paquetes del sistema
+sudo apt-get update
+
+# Instalar default-jdk
+sudo apt-get install default-jdk -y
+
+# Agregar claves del repositorio de Jenkins
+wget -qO - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+
+# Actualizar listado de paquetes del sistema
+sudo apt-get update
+
+# Instalar Jenkins server
+sudo apt-get install jenkins -y
+
+# Verificar estado servidor de Jenkins
+# sudo service jenkins status
+
+# Cat del fichero con el password inicial de Jenkins
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
---
+++ pozuelo-devops/installpython37.sh
@@ -0,0 +1,5 @@
+## Script Python 3.7 install
+## Jorge Pozuelo
+
+# Instalación python3.7 y virtualenv de python3.7
+sudo apt install python3.7 python3.7-venv
