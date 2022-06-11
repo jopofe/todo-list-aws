34695fd5d810cbd046552f691c00101d6203ce8f
Ubuntu <ubuntu@ip-172-31-88-125.ec2.internal>
2022-06-11 22:20:58 +0000

    Subir porcentaje de coverage

--- src/list.py
+++ src/list.py
@@ -3,14 +3,21 @@
 import json
 import decimalencoder
 import todoList
+from botocore.exceptions import ClientError
 
 
 def list(event, context):
-    # fetch all todos from the database
-    result = todoList.get_items()
-    # create a response
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(result, cls=decimalencoder.DecimalEncoder)
-    }
-    return response
+    try:
+        # fetch all todos from the database
+        result = todoList.get_items()
+        # create a response
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(result, cls=decimalencoder.DecimalEncoder)
+        }
+        
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        return response
+        
